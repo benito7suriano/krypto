7fbd65b6658e3e6901e592207a8401a2d85b8393
Beno Suriano <bjsuriano@gmail.com>
2022-08-29 12:35:24 -0400

    Update Transactions smart contract.

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,5 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
+  uint256 transactionCount;
 
+  // each parameter is of the form [type name].
+  event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+  // defining the TransferStruct "shape".
+  struct TransferStruct {
+    address sender;
+    address receiver;
+    uint amount;
+    string message;
+    uint256 timestamp;
+    string keyword;
+  }
+
+  // declaring transactions variable as an array of TransferStructs.
+  TransferStruct[] transactions;
+
+  // public defines the visibility
+  function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+    transactionCount += 1;
+		// msg object is something you immediately get when calling a function in the blockchain.
+		transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+		emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+  }
+  function getAllTransactions() public view returns (TransferStruct[] memory) {
+		return transactions;
+
+  }
+  function getTransactionCount() public view returns (uint256) {
+    return transactionCount;
+  }
 }
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,7 +1,17 @@
-require('@nomicfoundation/hardhat-toolbox')
+//eth-goerli.g.alchemy.com/v2/0KH_njLcXjIsEctsR0_jol0kkLDx0SbM
+
+https: require('@nomicfoundation/hardhat-toolbox')
 require('@nomicfoundation/hardhat-chai-matchers')
 require('@nomiclabs/hardhat-ethers')
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config()
+
 module.exports = {
   solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/0KH_njLcXjIsEctsR0_jol0kkLDx0SbM',
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+  },
 }
